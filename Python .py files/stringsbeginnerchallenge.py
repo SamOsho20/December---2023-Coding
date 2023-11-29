093b6bf41b580a815dc60d3cd92ae93f620a63f4
Sam_Osho <Sosho108@marian.edu>
2023-11-28 23:01:59 -0500

    Strings Interpolation beginner challenge

---
+++ Python .py files/stringsbeginnerchallenge.py
@@ -0,0 +1,25 @@
+# Practice Challenge: Correct the errors!
+first_name = "Alana"
+last_name = "Da Silva"
+age = 36
+profession = "Software Developer"
+years_experience = 5
+
+greeting = "Hello my name is "
+
+print(greeting, first_name, last_name) 
+# Desired output: Hello my name is Alana Da Silva
+
+print(f"I am {age} years old") 
+# Desired output: I am 36 years old
+
+print("I work as a",profession)
+# Desired output: I work as a Software Developer.
+
+exp_string = (f"I have worked in the field for {years_experience} years.")
+print(exp_string)
+# Desired output: I have worked in the field for 5 years.
+feild_start_age = age - years_experience
+print(feild_start_age)
+print(f"I started in the field when I was {feild_start_age} years old.")
+# Desired output: I started in the field when I was 31 years ol
