f82562468dac52b8fe0178421b3ddd243c8b11fc
Sam_Osho <Sosho108@marian.edu>
2023-11-29 22:39:26 -0500

    Building my knowledge on Built in Methods in lists for python

---
+++ Python .py files/Builtinmethodsinlist.py
@@ -0,0 +1,15 @@
+some_nums = [44,56,2,3,12,19,6]
+print(len(some_nums))
+print(max(some_nums))
+some_nums.append(44)
+print(some_nums)
+
+# Some optional challenges to assess and refine your understanding:
+
+# Print the length of the list.
+
+# Use antoher python built-in function and print the result.
+
+# Remove an item from the list. Remember to verify that it was removed.
+
+# Utilize a method from the documentation and print the result.
--- Python .py files/stringsbeginnerchallenge.py
+++ Python .py files/stringsbeginnerchallenge.py
@@ -7,19 +7,21 @@ years_experience = 5
 
 greeting = "Hello my name is "
 
-print(greeting, first_name, last_name) 
-# Desired output: Hello my name is Alana Da Silva
+# print(greeting, first_name, last_name) 
+# # Desired output: Hello my name is Alana Da Silva
 
-print(f"I am {age} years old") 
-# Desired output: I am 36 years old
+# print(f"I am {age} years old") 
+# # Desired output: I am 36 years old
 
-print("I work as a",profession)
-# Desired output: I work as a Software Developer.
+# print("I work as a",profession)
+# # Desired output: I work as a Software Developer.
 
-exp_string = (f"I have worked in the field for {years_experience} years.")
-print(exp_string)
-# Desired output: I have worked in the field for 5 years.
-feild_start_age = age - years_experience
-print(feild_start_age)
-print(f"I started in the field when I was {feild_start_age} years old.")
-# Desired output: I started in the field when I was 31 years ol
+# exp_string = (f"I have worked in the field for {years_experience} years.")
+# print(exp_string)
+# # Desired output: I have worked in the field for 5 years.
+# feild_start_age = age - years_experience
+# print(feild_start_age)
+# print(f"I started in the field when I was {feild_start_age} years old.")
+# # Desired output: I started in the field when I was 31 years old
+
+print(greeting.upper())
