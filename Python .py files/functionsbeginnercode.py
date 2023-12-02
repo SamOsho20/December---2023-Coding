2ba2b98504e96b28554859f573d76dbec77843bb
Sam_Osho <Sosho108@marian.edu>
2023-12-02 01:04:19 -0500

    Learning about functions with python!

---
+++ Python .py files/functionsbeginnercode.py
@@ -0,0 +1,15 @@
+# Challenge 1:
+#   Fill in the missing code for the full_name function.
+#   Be sure to add the 2 parameters (and name them appropriately)
+#   Return the full name to get the desired output!
+
+def full_name(first,last,age):
+    return (f"my name is {first} {last} I am {age} years old")
+
+name1 = full_name("Eddie", "Aikau",44)
+print(name1) # should print Eddie Aikau
+
+# Challenge 2: 
+name2 = full_name("sam", "osho",23)
+print(name2)
+#   Call the function again using your own name and print the results!
