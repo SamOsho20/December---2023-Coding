b4bf77fc3b5d7e82ffc4ccc38510dceab85b324e
Sam_Osho <Sosho108@marian.edu>
2023-12-02 22:45:39 -0500

    Addiing Dictonarys in Python

---
+++ Python .py files/AddingDictionarys.py
@@ -0,0 +1,17 @@
+# Challenge yourself!
+
+person = {"first_name": "Ada", "last_name": "Lovelace", "age": 42, "is_organ_donor": True}
+# Create a another person dictionary called person_2 and print it to the terminal
+person_2 = {"first_name": "sam", "last_name": "osho", "age": 23, "is_organ_donor": False}
+print(f"Hi,I am {person_2['first_name']} {person_2['last_name']}, I am {person_2['age']} it is {person_2['is_organ_donor']} that I am a organ Donor")
+capitals = {}
+capitals["svk"] = "Bratislava"
+capitals["deu"] = "Berlin"
+capitals["dnk"] = "Copenhagen"
+capitals["ngr"] = "Abuja"
+capitals['ind'] = "Indianapolis"
+# Add 2 key-value pairs to this dictionary.
+
+print(capitals)
+
+# print the capitals dictionary to see how it changed!
