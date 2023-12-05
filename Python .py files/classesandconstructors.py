6e932a119f0ddaf0410ff838e63c2e1d5ceefed3
Sam_Osho <Sosho108@marian.edu>
2023-12-04 23:22:57 -0500

    Learning about constructors and classes in Python!

---
+++ Python .py files/classesandconstructors.py
@@ -0,0 +1,23 @@
+class User:		
+    def __init__(self):
+        self.first_name = "Ada"
+        self.last_name = "Lovelace"
+        self.age = 42
+
+user_ada = User()
+print(user_ada.first_name)
+user_2=User()
+print(user_ada.last_name)
+print(user_2.last_name)
+# Create another user called user_2!
+
+# Print user_ada's last name.
+# Print user_2's last name. (Yes, they should be the same)
+
+# Run the code, pause, go back and step through one line at a time
+# What do you notice about the order it runs in?
+# Write down any other questions you have.
+
+# Sensei Exercise: try just printing the variable, user_ada.
+#   What prints to the terminal?
+
