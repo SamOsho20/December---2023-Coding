49123caeb3a55b1f3d2b759e28aa0d415d219343
Sam_Osho <Sosho108@marian.edu>
2023-12-04 23:35:13 -0500

    Passing in arguments for classes  in python!

---
+++ Python .py files/passingargumentsinpython.py
@@ -0,0 +1,33 @@
+class Shoe:
+    # now our method has 4 parameters (including self)!
+    def __init__(self, brand, shoe_type, price):
+    	# we assign them accordingly
+        self.brand = brand
+        self.type = shoe_type
+        self.price = price
+        # the status is set to True by default
+        self.in_stock = True
+
+# skater_shoe = Shoe("Vans", "Low-top Trainers", 59.99)
+# dress_shoe = Shoe("Jack & Jill Bootery", "Ballet Flats", 29.99)
+# print(skater_shoe.type)	# output: Low-top Trainers
+# print(dress_shoe.type)	# output: Ballet Flats
+
+class shoe2:
+  # now our method has 4 parameters (including self)!
+    def __init__(self, brand, shoe_type, price):
+    	# we assign them accordingly
+        self.brand = brand
+        self.type = shoe_type
+        self.price = price
+        # the status is set to True by default
+        self.in_stock = True
+new_balance= shoe2("New Balance", "runners", 120)
+print(new_balance.price)
+new_balance.in_stock = False
+print(new_balance.in_stock)
+
+# Ninja Challenges!
+# Open this code on the Trace website to get a better view of all the variables and their attributes.
+# Make a new instance of a shoe
+# Update the in_stock attribute to False
